ddaa4deecf09e195e86865f4fdc32d80cce4ca2b
Marius Andra <marius.andra@gmail.com>
2021-04-26 09:42:07 +0200

    add kea packages and demo page

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.scss'
 
 function App() {
   return (
@@ -10,17 +10,12 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="https://kea.js.org" target="_blank" rel="noopener noreferrer">
+          Learn Kea
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { getContext, resetContext } from 'kea'
+import { routerPlugin } from 'kea-router'
+import { loadersPlugin } from 'kea-loaders'
+import { Provider } from 'react-redux'
+
+resetContext({
+  plugins: [routerPlugin, loadersPlugin],
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={getContext().store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
