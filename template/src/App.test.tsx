1f9e033e316cfb3f1f95a3f1d68bed52db1ec19f
Marius Andra <marius.andra@gmail.com>
2021-10-24 00:25:21 +0200

    simplify component test

--- template/src/App.test.tsx
+++ template/src/App.test.tsx
@@ -1,20 +1,16 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render as _render, screen } from '@testing-library/react'
 import App from './App'
 import { initKea } from './initKea'
 import { Provider } from 'kea'
 
+const render = (element: React.ReactElement) => _render(<Provider>{element}</Provider>)
+
 describe('App', () => {
-  beforeEach(() => {
-    initKea()
-  })
+  beforeEach(initKea)
 
   test('renders learn kea link', () => {
-    render(
-      <Provider>
-        <App />
-      </Provider>
-    )
+    render(<App />)
     const linkElement = screen.getByText(/learn kea/i)
     expect(linkElement).toBeInTheDocument()
   })
