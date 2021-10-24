a8dde43cb09a68e204883485bdd30b642c78c2e8
Marius Andra <marius.andra@gmail.com>
2021-10-24 11:41:19 +0200

    mock the router as needed, add test utils plugin

--- template/src/initKea.ts
+++ template/src/initKea.ts
@@ -1,9 +1,22 @@
 import { resetContext } from 'kea'
 import { routerPlugin } from 'kea-router'
 import { loadersPlugin } from 'kea-loaders'
+import { testUtilsPlugin } from 'kea-test-utils'
+import { createMemoryHistory } from 'history'
+
+const IS_TEST_MODE = process.env.NODE_ENV === 'test'
 
 export function initKea() {
+  const routerOptions: Record<string, any> = {}
+  if (IS_TEST_MODE) {
+    const history = createMemoryHistory()
+    ;(history as any).pushState = history.push
+    ;(history as any).replaceState = history.replace
+    routerOptions.history = history
+    routerOptions.location = history.location
+  }
+
   resetContext({
-    plugins: [routerPlugin, loadersPlugin],
+    plugins: [routerPlugin(routerOptions), loadersPlugin].concat(IS_TEST_MODE ? [testUtilsPlugin] : []),
   })
 }
