4149edc212b5ff3eef7b7468b98d67c8c03bc662
Marius Andra <marius.andra@gmail.com>
2022-04-24 00:29:53 +0200

    correct url

--- template/src/App/App.tsx
+++ template/src/App/App.tsx
@@ -16,7 +16,7 @@ export function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a className="App-link" href="https://kea.js.org" target="_blank" rel="noopener noreferrer">
+        <a className="App-link" href="https://keajs.org" target="_blank" rel="noopener noreferrer">
           Learn Kea
         </a>
       </header>
