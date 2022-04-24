2e45be06e51a3015f7d66efff52ee2f15a171b35
Marius Andra <marius.andra@gmail.com>
2022-04-24 14:21:23 +0200

    newer logic and packages

--- template/src/App/appLogic.ts
+++ template/src/App/appLogic.ts
@@ -1,11 +1,12 @@
-import { kea } from 'kea'
+import { kea, actions, reducers, path } from 'kea'
 import { appLogicType } from './appLogicType'
 
-export const appLogic = kea<appLogicType>({
-  actions: {
+export const appLogic = kea<appLogicType>([
+  path(['App', 'appLogic']),
+  actions({
     goFaster: true,
-  },
-  reducers: {
+  }),
+  reducers({
     speed: [1, { goFaster: (state) => (state + 1) % 5 }],
-  },
-})
+  }),
+])
